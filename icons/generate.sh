73cdf5a73a754ed6626be2c0bfe06a3cc10c7070
Igor Sharapov <igorsharapov@outlook.com>
2020-11-10 16:36:43 +0300

    Enable theme structure

--- icons/generate.sh
+++ icons/generate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+themename="maintheme" # switch to loop if more themes needed
 svgdir="svg"
-icons=`cd $svgdir && ls *.svg`
+icons=`cd $themename/$svgdir && ls *.svg`
 tab="    "
 iconsres="../icons.qrc"
 
@@ -9,19 +10,41 @@ iconsres="../icons.qrc"
 echo "<RCC>" > $iconsres
 echo "${tab}<qresource prefix=\"/\">" >> $iconsres
 
-for resolution in 16 32 64
+themefile=${themename}/index.theme
+echo "${tab}${tab}<file>icons/${themename}/index.theme</file>" >> $iconsres
+
+echo "[Icon Theme]" > $themefile
+echo "Name=${themename}" >> $themefile
+echo "Comment=${themename}" >> $themefile
+
+directories="Directories="
+
+resolutions=(16 32 64)
+
+for resolution in ${resolutions[@]}
 do
-    dirname="${resolution}x${resolution}"
+    directories="${directories}${resolution}x${resolution},"
+done
+echo "${directories%?}" >> $themefile
+for resolution in ${resolutions[@]}
+do
+    echo "" >> $themefile
+    resdir="${resolution}x${resolution}"
+    echo "[${resdir}]" >> $themefile
+    echo "Size=${resolution}" >> $themefile
+    echo "Type=Fixed" >> $themefile
+    dirname="${themename}/${resdir}"
     echo $dirname
     rm -rf $dirname
     mkdir -p $dirname
     for icon in $icons
     do
-        pngpath=$dirname/${icon/svg/png}
-        inkscape -w $resolution -h $resolution $svgdir/$icon --export-filename $pngpath 2>/dev/null
+        pngpath="$dirname/${icon/svg/png}"
+        inkscape -w $resolution -h $resolution $themename/$svgdir/$icon --export-filename $pngpath 2>/dev/null
         echo "${tab}${tab}<file>icons/${pngpath}</file>" >> $iconsres
     done
 done
 
 echo "${tab}</qresource>" >> $iconsres
 echo "</RCC>" >> $iconsres
+
