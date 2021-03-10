5f77bf59327e5672d42f6a230aa751ec78d9cf69
Igor Sharapov <igorsharapov@outlook.com>
2021-03-10 18:25:29 +0300

    Change qttest main app type

--- Tests/QtPiDeck/ViewModels/SettingsViewModelTests/main.cpp
+++ Tests/QtPiDeck/ViewModels/SettingsViewModelTests/main.cpp
@@ -181,6 +181,6 @@ void SettingsViewModelTests::testDisabledIfConnectedToSameServer() {
 }
 }
 
-QTEST_MAIN(QtPiDeck::ViewModels::Tests::SettingsViewModelTests) // NOLINT
+QTEST_GUILESS_MAIN(QtPiDeck::ViewModels::Tests::SettingsViewModelTests) // NOLINT
 
 #include "main.moc"
